01934eac20988c4fb2597d815224297a093fe3ba
mbenbel <manel.ben-belgacem@adp.com>
2020-04-12 17:13:37 +0200

    💦[C4] Ajout d’une directive

--- src/app/modules/articles/articles.module.ts
+++ src/app/modules/articles/articles.module.ts
@@ -1,11 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { DynamicLabelDirective } from 'src/app/dynamic-label.directive';
 import { FibonacciPipe } from 'src/app/fibonacci.pipe';
 
 import { ArticleFormComponent } from './components/article-form/article-form.component';
 import { ArticleItemVotesComponent } from './components/article-item-votes/article-item-votes.component';
 import { ArticleItemComponent } from './components/article-item/article-item.component';
+import { ArticleMetaComponent } from './components/article-meta/article-meta.component';
 import { ArticlesComponent } from './containers/articles/articles.component';
 import { ArticlesService } from './services/articles.service';
 
@@ -15,7 +17,9 @@ import { ArticlesService } from './services/articles.service';
     ArticleItemComponent,
     ArticleItemVotesComponent,
     ArticlesComponent,
-    FibonacciPipe
+    ArticleMetaComponent,
+    FibonacciPipe,
+    DynamicLabelDirective
   ],
   imports: [CommonModule, FormsModule],
   exports: [ArticlesComponent],
