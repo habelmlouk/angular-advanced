20cc99e38f8b9d98795371aa77aa66fdb325c32c
mbenbel <manel.ben-belgacem@adp.com>
2020-05-05 00:56:22 +0200

    👩‍🏭[mbenbel-C7]: Formulaire login avec template driven

--- src/app/modules/articles/components/article-form/article-form.component.ts
+++ src/app/modules/articles/components/article-form/article-form.component.ts
@@ -12,13 +12,17 @@ import {
   selector: 'app-article-form',
   templateUrl: './article-form.component.html',
   styleUrls: ['./article-form.component.scss'],
+  //ce component est statique: n'est pas impacté par le reste, donc il ne change pas car aucun @Input
   changeDetection: ChangeDetectionStrategy.OnPush // better to have it even no inputs here
 })
 export class ArticleFormComponent implements OnInit {
   @Output() addedArticle = new EventEmitter<{ title: string; link: string }>();
 
+  // accéder à un elt dans HTML
   @ViewChild('title', { static: true }) title: ElementRef;
   @ViewChild('link', { static: false }) link: ElementRef;
+
+  placeholder = 'manel';
 
   ngOnInit(): void {
     console.log('static true: title is available', this.title);
@@ -31,6 +35,7 @@ export class ArticleFormComponent implements OnInit {
   addArticle(title: string, link: string): void {
     if (title && link) {
       this.addedArticle.emit({ title, link });
+      // faut mettre nativeElement pour accéder au DOM
       this.title.nativeElement.value = '';
       this.link.nativeElement.value = '';
     }
--- src/app/modules/auth/modules/login/components/login-form/login-form.component.ts
+++ src/app/modules/auth/modules/login/components/login-form/login-form.component.ts
@@ -1,8 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-auth-login',
   templateUrl: './login-form.component.html',
   styleUrls: ['./login-form.component.scss']
 })
-export class LoginFormComponent {}
+export class LoginFormComponent {
+  @ViewChild('f', { static: false }) form: FormGroup;
+  messageGlobal: string = '';
+
+  constructor(private router: Router) { }
+
+  onLogin(): void {
+    const { email, password } = this.form.value;
+    console.log(email);
+    console.log(password);
+    this.form.reset();
+    this.messageGlobal = 'Login ou mot de passe incorrect';
+  }
+}
